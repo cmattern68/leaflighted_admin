e883dad05cc335b02064c13f3e938de1c4c6fa24
corentin.mattern <corentin.mattern@epitech.eu>
2019-05-21 17:46:53 +0200

    logout

--- interface/pages/logout.php
+++ interface/pages/logout.php
@@ -1,5 +1,9 @@
 <?php
 if (preg_match("/page/i", $_SERVER['REQUEST_URI']) && !preg_match("/index.php/i", $_SERVER['REQUEST_URI']))
     echo "<script type=\"text/javascript\">window.location.href = 'index.php?page=home';</script>";
-echo "logout";
+if (isset($_SESSION['user_id']))
+    unset($_SESSION['user_id']);
+$user = null;
+session_destroy();
+echo "<script type=\"text/javascript\">window.location.href = '../index.php';</script>";
 ?>
