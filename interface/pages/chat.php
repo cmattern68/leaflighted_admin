ed9a89f5e7ab04254ecd37f1102b2cf7c1a1999f
corentin.mattern <corentin.mattern@epitech.eu>
2019-05-21 17:42:15 +0200

    templating - refacto - gestion des utilisateur - logeur général - ajout du index.php?page=

---
+++ interface/pages/chat.php
@@ -0,0 +1,5 @@
+<?php
+if (preg_match("/page/i", $_SERVER['REQUEST_URI']) && !preg_match("/index.php/i", $_SERVER['REQUEST_URI']))
+    echo "<script type=\"text/javascript\">window.location.href = 'index.php?page=home';</script>";
+echo "chat";
+?>
